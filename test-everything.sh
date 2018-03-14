d15c7cbffd3c9d6190dfeaacdb5da8a54cf3e230
Julian Lettner <julian.lettner@gmail.com>
2018-03-13 17:22:45 -0700

    Add scripts for running fuzzers serially on (ava) instead of parallel (in the cloud)

--- test-everything.sh
+++ test-everything.sh
@@ -4,11 +4,11 @@
 
 . $(dirname $0)/common.sh
 
-PARENT_DIR="RUN_EVERY_BENCHMARK"
-#[[ -e "$PARENT_DIR" ]] && echo "Rename folder $PARENT_DIR to avoid deletion" && exit 1
-rm -rf $PARENT_DIR
-mkdir $PARENT_DIR
-echo "Created top directory $PARENT_DIR"
+# PARENT_DIR="RUN_EVERY_BENCHMARK"
+# #[[ -e "$PARENT_DIR" ]] && echo "Rename folder $PARENT_DIR to avoid deletion" && exit 1
+# rm -rf $PARENT_DIR
+# mkdir $PARENT_DIR
+# echo "Created top directory $PARENT_DIR"
 
 ABS_SCRIPT_DIR=$(readlink -f $SCRIPT_DIR)
 BENCHMARKS=${ABS_SCRIPT_DIR}/*/
@@ -18,7 +18,8 @@ do
   file_name="$(basename $f)"
   [[ ! -d $f ]] && continue # echo "${file_name} isn't a directory" && continue
   [[ ! -e ${f}build.sh ]] && continue # echo "${file_name} has no build script" && continue
-  echo "Running build_and_test $file_name"
-  (cd $PARENT_DIR && ${ABS_SCRIPT_DIR}/build-and-test.sh "${file_name}" > from-${file_name}.out 2>&1  &) # && sleep 10
+  echo "Running test $file_name"
+  # (cd $PARENT_DIR && ${ABS_SCRIPT_DIR}/build-and-test.sh "${file_name}" > from-${file_name}.out 2>&1  &) # && sleep 10
+  (${ABS_SCRIPT_DIR}/test-only.sh "${file_name}" > run-${file_name}.out 2>&1  && sleep 5)
 done
 
