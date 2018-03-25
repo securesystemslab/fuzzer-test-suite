1e6b536c08d3d7fb1ae9ff9df88eda95f29da194
Julian Lettner <julian.lettner@gmail.com>
2018-03-25 01:37:22 -0700

    Generate pretty (and correct) graphs

---
+++ graph/pretty_plot.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import matplotlib.pyplot as plt
+from scipy.stats.mstats import gmean
+
+configs = ['baseline', 'simpler']
+config_labels = ['Baseline', 'PartiSan']
+config_linestyles = ['--', '-', '-.']
+benchmarks = ['boringssl-2016-02-12', 'guetzli-2017-3-30', 'harfbuzz-1.3.2',
+'json-2017-02-12', 'lcms-2017-03-21', 'libarchive-2017-01-04', 'libjpeg-turbo-07-2017',
+'libpng-1.2.56', 'openssl-1.1.0c', 'openthread-2018-02-27', 'sqlite-2016-11-14',
+'woff2-2016-05-06', 'wpantund-2018-02-27']
+types = ['coverage', 'features', 'corpus-units', 'corpus-size', 'exec-s', 'rss']
+type_labels = ['coverage', 'features', 'corpus units', 'corpus size', 'exec/s', 'rss']
+num_files = 10
+
+
+def compute_data(cfg, bench):
+    file_scheme = cfg + '_' + bench + '_'
+    files = [open(file_scheme+str(i)+'.csv') for i in range(num_files)]
+
+    # data types -> series
+    data = [[] for _ in types]
+
+    while files:
+        finished_files = []
+        # Collect related values across files
+        values = [[] for _ in types]
+        for f in files:
+            line = f.readline()
+            if line:
+                line_values = line.split()
+                assert len(line_values) == len(types)
+                for i in range(len(types)):
+                    val = int(line_values[i])
+                    assert val > 0 or i == 4    # exec/s might be zero which is a problem for geomean
+                    val = max(val, 1)
+                    values[i].append(val)
+            else:
+                finished_files.append(f)
+
+        # Remove exhausted files
+        for f in finished_files:
+            files.remove(f)
+            f.close()
+
+        if files:
+            gms = [gmean(val) for val in values]
+            for i in range(len(types)):
+                data[i].append(gms[i])
+
+    return data
+
+
+for bench in benchmarks:
+    # config -> data types -> series
+    all_data = [compute_data(cfg, bench) for cfg in configs]
+    for t in range(len(types)):
+        plt.figure(figsize=(8, 4.5))  # default: (8, 6)
+        plt.ylabel(type_labels[t])
+        plt.xlabel('minutes')
+
+        for c in range(len(configs)):
+            series = all_data[c][t]
+            plt.plot(series, label=config_labels[c], linewidth=1.2, linestyle=config_linestyles[c])
+
+        plt.legend(loc='lower right', shadow=True)
+        file_name = bench + '_' + types[t] + '_gmean.pdf'
+        plt.savefig(file_name, bbox_inches='tight')
+        plt.close()
+
