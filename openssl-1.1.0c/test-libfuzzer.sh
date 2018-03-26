e096bdbacd0a00911146f898da52bd0640eb19f7
Julian Lettner <julian.lettner@gmail.com>
2018-03-26 11:10:32 -0700

    Choose right fuzzer

--- openssl-1.1.0c/test-libfuzzer.sh
+++ openssl-1.1.0c/test-libfuzzer.sh
@@ -9,5 +9,5 @@ mkdir $CORPUS
 rm -r seeds-x509
 cp -r SRC/fuzz/corpora/x509 seeds-x509
 
-[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -seed=$SEED -max_total_time=$MAX_TOTAL_TIME -reload=$RELOAD -verbosity=$VERBOSITY -print_new_units=$PRINT_NEW_UNITS -print_pcs=$PRINT_PCS -print_funcs=$PRINT_FUNCS -print_final_stats=$PRINT_FINAL_STATS -print_corpus_stats=$PRINT_CORPUS_STATS -print_coverage=$PRINT_COVERAGE -artifact_prefix=$CORPUS/  -jobs=$JOBS -workers=$JOBS $CORPUS seeds-x509
+[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE-x509 -seed=$SEED -max_total_time=$MAX_TOTAL_TIME -reload=$RELOAD -verbosity=$VERBOSITY -print_new_units=$PRINT_NEW_UNITS -print_pcs=$PRINT_PCS -print_funcs=$PRINT_FUNCS -print_final_stats=$PRINT_FINAL_STATS -print_corpus_stats=$PRINT_CORPUS_STATS -print_coverage=$PRINT_COVERAGE -artifact_prefix=$CORPUS/  -jobs=$JOBS -workers=$JOBS $CORPUS seeds-x509
 # no bug
